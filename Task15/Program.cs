604acf868b02bf18fde1daa48b335bf4d6ac0c3c
Mikhail Latypov <mikhaillat@yandex.ru>
2023-02-10 12:59:25 +0300

    Add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15. Напишите задачу, которая
+// 1. принимает на вход цифру, обозначающую
+// 2. день недели, и проверяет, является ли
+// 3. этот день выходным.
+
+bool GetWeekEnd(int num)
+{
+    return num == 6 || num == 7;
+}
+
+Console.Write("Введите порядковый номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 1 || number > 7)
+{
+    Console.Write("Вы ввели неправильный порядковый номер! Введите число от 1 до 7: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+bool res = GetWeekEnd(number); // вызов метода
+Console.WriteLine(res ? "является выходным днём" : "не является выходным днём");
