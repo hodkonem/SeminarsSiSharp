6a01961154120b98e802875fdbed1f42f930cb72
Mikhail Latypov <mikhaillat@yandex.ru>
2023-02-10 13:00:30 +0300

    Add Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,18 @@
+﻿bool MultipleNum(int num1)
+{
+    return num1 % 7 == 0 && num1 % 23 == 0;
+}
+
+Console.WriteLine("Введите число :");
+
+int number = Convert.ToInt32(Console.ReadLine());
+bool remains = MultipleNum(number);
+
+if (remains == true)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
