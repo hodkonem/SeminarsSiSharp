d098cf5006f0e60ff0a19b8dc2d9d8dd1d8d05c4
Mikhail Latypov <mikhaillat@yandex.ru>
2023-02-10 12:58:18 +0300

    Add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 14. 
+// Напишите программу, 
+// которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool Multiple(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+Console.WriteLine("Введите число :");
+
+int number = Convert.ToInt32(Console.ReadLine());
+bool result = Multiple(number);
+
+if (result == true)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
