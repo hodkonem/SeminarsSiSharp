4106f8972105282a33005bf733b8489cc43b41ac
Mikhail Latypov <mikhaillat@yandex.ru>
2023-02-10 13:45:31 +0300

    Add Task52

---
+++ Task52/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 52. 
+// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+double[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+// Далее создаём метод для вывода матрицы
+string PrintMatrix(double[,] matrix)
+{
+    string str = string.Empty;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        str += "[";
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == matrix.GetLength(1) - 1) str += $" {matrix[i, j],3} ";
+            else str += $" {matrix[i, j],3} ";
+        }
+        str += "j\n";
+    }
+    return str;
+}
+
+string PrintArray(double[] arr)
+{
+    string str = "[";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (str == "[") str += arr[i];
+        else str += $", {arr[i]}";
+    }
+    str += "]";
+    return str;
+}
+
+double[] GetArithmeticMeanRows(double[,] matrix)
+{
+    double[] arrayArithmeticMeanRows = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        arrayArithmeticMeanRows[j] = Math.Round(sum / matrix.GetLength(0), 1);
+    }
+    return arrayArithmeticMeanRows;
+}
+
+double[,] matrixNumbers = GetMatrix(3, 4, 0, 10);
+Console.WriteLine(PrintMatrix(matrixNumbers));
+
+double[] arithmeticMeanRows = GetArithmeticMeanRows(matrixNumbers);
+Console.WriteLine(PrintArray(arithmeticMeanRows));
