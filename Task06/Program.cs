a7a80ad3a90322f2b82dc88e85c4a1d0cf6f13fa
Mikhail Latypov <mikhaillat@yandex.ru>
2023-02-15 08:15:56 +0300

    Add Task06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -16,9 +16,9 @@ Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number % 2 == 0)
 {
-    Console.WriteLine("Четное");
+    Console.WriteLine($"Число {number} является чётным");
 }
 else
 {
-    Console.WriteLine("Нечетное");
+    Console.WriteLine($"Число {number} не является чётным");
 }
