1b9998de499b77789fbf9b5db6bb21e70bbddc00
Mikhail Latypov <mikhaillat@yandex.ru>
2023-02-17 12:01:59 +0300

    Add Task51

--- Task51/Program.cs
+++ Task51/Program.cs
@@ -22,3 +22,38 @@ int[,] GenerateMatrixRndInt(int rows, int columns, int min, int max)
     }
     return matrix;
 }
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3} | ");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.Write("|\n");
+    }
+}
+
+int DiagonalValueSum(int[,] matrix)
+{
+    int sum = default;
+    int size = matrix.GetLength(0);
+    if (matrix.GetLength(0) > matrix.GetLength(1)) size = matrix.GetLength(1);
+
+    for (int i = 0; i < size; i++)
+    {
+        sum += matrix[i, i];
+    }
+    return sum;
+}
+
+int[,] matr = GenerateMatrixRndInt(6, 5, 0, 10);
+PrintMatrix(matr);
+int diagonalValueSum = DiagonalValueSum(matr);
+Console.WriteLine();
+Console.WriteLine($"Sum of diagonal values is {diagonalValueSum}");
+
+
