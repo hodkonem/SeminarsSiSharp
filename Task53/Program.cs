55b2891e760fbd7393aa426ba6da911f6a3f6f6d
Mikhail Latypov <mikhaillat@yandex.ru>
2023-02-19 00:20:26 +0300

    Add Task55

--- Task53/Program.cs
+++ Task53/Program.cs
@@ -15,6 +15,7 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
     }
     return matrix;
 }
+
 void Replace(int[,] matrix)
 {
     int tmp = 0;
@@ -25,6 +26,7 @@ void Replace(int[,] matrix)
         matrix[matrix.GetLength(0) - 1, i] = tmp;
     }
 }
+
 void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -37,6 +39,7 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine();
     }
 }
+
 int[,] matrix = CreateMatrixRndInt(3, 4, 1, 10);
 PrintMatrix(matrix);
 Console.WriteLine();
