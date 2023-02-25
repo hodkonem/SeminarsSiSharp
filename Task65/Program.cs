dc488df26f3467702e58a8c3f3c1bf02ae85fb32
Mikhail Latypov <mikhaillat@yandex.ru>
2023-02-25 10:57:00 +0300

    Add Task65

--- Task65/Program.cs
+++ Task65/Program.cs
@@ -3,15 +3,13 @@
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
 
-Console.WriteLine("Введите два числа: ");
+Console.WriteLine("Введите два числа:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
-
 GapNaturalNumbers(number1, number2);
-
 void GapNaturalNumbers(int num1, int num2)
 {
-    Console.Write($"{num1}");
+    Console.Write($"{num1} ");
     if (num1 == num2) return;
     if (num1 > num2)
     {
