46b10786672acbe1b3cb48cb4d7cc30df8ab58f4
Mikhail Latypov <mikhaillat@yandex.ru>
2023-02-26 11:03:41 +0300

    Add Task69

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -14,3 +14,9 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int sumDigits = SumDigits(number);
 Console.WriteLine(sumDigits);
+
+// This code calculates the sum of all digits in a given number. 
+// The function SumDigits() takes an integer parameter and checks if the number is equal to 0, if it is, then it returns 0. 
+// If the number is not equal to 0, the result of the function is equal to the remainder of num divided by 10 plus the result of SumDigits(num / 10). 
+// The main program reads the user input and converts it into an integer. 
+// After that, it calls the SumDigits() function to calculate the sum of digits of the given number and prints the result.
---
+++ Task69/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// int SumDigits(int num1, int num2)
+// {
+// return num2 == 0 ? 1 : num1 * SumDigits(num1, num2 - 1);
+// }
+
+// Console.Write("Введите число A :");
+// int numberOne = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B :");
+// int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+// int numberOneAExtentNumberTwo = SumDigits(numberOne,numberTwo);
+// Console.WriteLine(numberOneAExtentNumberTwo);
+
+// Оптимизированное решение
+Console.WriteLine("введите число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int prodactNumbersAB = ProdactNumbersAB(numA, numB);
+Console.WriteLine($"число {numA} в целую степень {numB} равно {prodactNumbersAB}");
+
+
+int PowerRec(int numberA, int numberB)
+{
+    return numberB == 0 ? 1 : PowerRec(numberA, numberB - 1) * numberA;
+
+}
+
+int ProdactNumbersAB(int numberA, int numberB)
+{
+    if (numberB == 0) return 1;
+    if (numberB % 2 == 0) return ProdactNumbersAB(numberA * numberA, numberB / 2);
+    return PowerRec(numberA, numberB - 1) * numberA;
+}
